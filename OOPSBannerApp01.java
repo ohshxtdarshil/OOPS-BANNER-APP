1a22a8e263d01f34e02ea31b37f0e93ccf42e7de
ohshxtdarshil <darshil16283@gmail.com>
2026-02-22 22:33:43 +0530

    Add files via upload

---
+++ OOPSBannerApp01.java
@@ -0,0 +1,24 @@
+public class OOPSBannerApp01 {
+    public static void main(String[] args) {
+        // Line 1 of the banner
+        System.out.println("  *** " + "  *** " + " ***** " + "  ***** ");
+        
+        // Line 2
+        System.out.println(" * * " + " * * " + " * * " + " * ");
+        
+        // Line 3
+        System.out.println(" * * " + " * * " + " * * " + " * ");
+        
+        // Line 4
+        System.out.println(" * * " + " * * " + " ***** " + "  **** ");
+        
+        // Line 5
+        System.out.println(" * * " + " * * " + " * " + "      * ");
+        
+        // Line 6
+        System.out.println(" * * " + " * * " + " * " + "      * ");
+        
+        // Line 7
+        System.out.println("  *** " + "  *** " + " * " + " ***** ");
+    }
+}
